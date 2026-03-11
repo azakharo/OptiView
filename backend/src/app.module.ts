ed86d1ed0c703b05feea1784a8476ccf6c3bc499
Alexey Zakharov <alexey.a.zakharov@gmail.com>
2026-03-11 10:49:15 +0300

    fixed missing swagger doc

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { dataSourceOptions } from './data-source';
+import { ImageModule } from './modules/images/image.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(dataSourceOptions)],
+  imports: [TypeOrmModule.forRoot(dataSourceOptions), ImageModule],
   controllers: [AppController],
   providers: [AppService],
 })
