83beaad4452abe7260f1317334517bd6f81c6a58
Alexey Zakharov <alexey.a.zakharov@gmail.com>
2026-03-14 17:22:33 +0300

    fixed test handing at the end

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,15 +1,21 @@
-import {defineConfig, mergeConfig} from 'vitest/config';
-import viteConfig from './vite.config';
+import * as path from 'node:path';
+import {defineConfig} from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig((env) =>
-  mergeConfig(viteConfig(env), {
-    test: {
-      globals: true,
-      environment: 'jsdom',
-      setupFiles: ['./src/test/setup.ts'],
-      css: true,
-      mockReset: true,
-      include: ['src/**/*.{test,spec}.{js,ts,jsx,tsx}'],
+export default defineConfig((env) => ({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
-  }),
-);
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/test/setup.ts'],
+    css: true,
+    mockReset: true,
+    include: ['src/**/*.{test,spec}.{js,ts,jsx,tsx}'],
+    fileParallelism: false,
+  },
+}));
